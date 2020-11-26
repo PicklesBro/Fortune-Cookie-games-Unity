6eebe0f37be66c11b650d4b42a57d6e89b4c4ca4
Jakub Kulawiak <Jakub.Kulawiak@georgebrown.ca>
2020-11-26 14:46:11 -0500

    Animations re-added

---
+++ Chrono Dash/Assets/Movement.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    public float speed = 10f;
+    public Animator animator;
+    public bool facingRight = true;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        float horizontalMove = Input.GetAxis("Horizontal");
+        animator.SetFloat("Speed", horizontalMove);
+      
+        Jump();
+        Vector3 movement = new Vector3(Input.GetAxis("Horizontal"), 0f, 0f);
+        transform.position += movement * Time.deltaTime * speed;
+        
+        if (horizontalMove > 0 &&! facingRight)
+            Flip();
+
+        else if (horizontalMove < 0 && facingRight)
+            Flip();
+        
+    }
+    void Jump()
+    {
+        if (Input.GetButtonDown("Jump"))
+        {
+            gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(0f, 20f), ForceMode2D.Impulse);
+        }
+    }
+
+    void Flip()
+    {
+        facingRight = !facingRight;
+        Vector4 theScale = transform.localScale;
+        theScale.x *= -1;
+        transform.localScale = theScale;
+    }
+}
